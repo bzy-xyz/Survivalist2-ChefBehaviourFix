a9bcb9f7fc6ab30368b83a42806d538f9209b70b
Ben Z Yuan <bzy.xyz@gmail.com>
2020-11-22 14:26:49 -0800

    ChefBehaviourFix: init
    By default, when a survivor with Cook role finishes cooking a recipe
    at a campfire, they become stuck staring at the campfire instead of
    clearing out the finished food. (They also forget what they were cooking
    in the first place.)
    
    We change this behaviour here to make this role work much more
    effectively, by preventing these survivors from forgetting their recipe
    in this case, and encouraging them to put away the newly cooked food.
    
    As this version uses transpiling to accomplish this goal, consider this
    version unstable.

---
+++ README.md
@@ -0,0 +1,42 @@
+Chef Behaviour Fix
+===================
+
+A mod for Survivalist: Invisible Strain.
+
+By default, when a survivor with Cook role finishes cooking a recipe
+at a campfire, they become stuck staring at the campfire instead of
+clearing out the finished food. (They also forget what they were cooking
+in the first place.)
+
+We change this behaviour here to make this role work much more
+effectively, by preventing these survivors from forgetting their recipe
+in this case, and encouraging them to put away the newly cooked food.
+
+Note that chefs will repeat the same recipe until your community runs
+out of ingredients or hits the crafting limit for the product, or the
+chef gives up for some other reason. You may want to set up limits
+beforehand (via scheduling and then canceling recurrent craft orders)
+to make it less likely that you'll cook _all_ of your corn / pumpkins
+without noticing, especially in winter when you can't plant anything.
+
+There are still rare cases where chefs will stare into space without a
+particular reason. As a potential workaround, manually queue a recurring
+recipe and then have the chef resume their role.
+
+Requirements
+------------
+
+* Unity Mod Manager installed into Survivalist: Invisible Strain.
+
+Build
+-----
+
+1. Checkout to `dev/ChefBehaviourFix`.
+2. From the `dev/ChefBehaviourFix/src` directory, run `dotnet build`.
+3. Copy `dev/ChefBehaviourFix/src/bin/Debug/net40/ChefBehaviourFix.dll` 
+   and `dev/ChefBehaviourFix/Info.json` to `Mods/ChefBehaviourFix`.
+
+License
+-------
+
+I claim nothing. ¯\_(ツ)_/¯
---
+++ src/ChefBehaviourFix.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using HarmonyLib;
+using UnityModManagerNet;
+
+namespace ChefBehaviourFix
+{
+    static class Main
+    {
+        public static UnityModManager.ModEntry mod;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Harmony.DEBUG = true;
+
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll();
+
+            mod = modEntry;
+
+            return true;
+        }      
+    }
+
+    [HarmonyPatch]
+    static class CraftGoal__ReversePatch {
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(CraftGoal), "HasUsedIngredientsWaitingForProduct")]
+        public static bool HasUsedIngredientsWaitingForProduct(CraftGoal instance, Character character) {
+            throw new NotImplementedException("stub");
+        }
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(CraftGoal), "GetRetrieveFinishedItemGoal")]
+        public static bool GetRetrieveFinishedItemGoal(CraftGoal instance, Character character, CraftingProp craftingProp, out Goal goal) {
+            throw new NotImplementedException("stub");
+        }
+    }
+    
+    [HarmonyPatch(typeof(CraftGoal), "GetNextSubGoal")]
+    static class CraftGoal__GetNextSubGoal__Patch {
+        static Goal Postfix(Goal __result, ref CraftGoal __instance, Character character, Goal parent) {
+            // System.Text.StringBuilder goal_sb = new System.Text.StringBuilder();
+            // System.Text.StringBuilder result_goal_sb = new System.Text.StringBuilder();
+            // __instance.BuildDebugString(goal_sb);
+            if (__result != null) {
+                // __result.BuildDebugString(result_goal_sb);
+            } else {
+                // result_goal_sb.Append("null");
+            }
+            // Main.mod.Logger.Warning($"a {character.FirstName} {character.Surname} {goal_sb.ToString()} => {result_goal_sb.ToString()}");
+            if (__instance.FollowingRecipe == null) {
+                // Main.mod.Logger.Warning("no recipe!");
+            }
+            else {
+                // Main.mod.Logger.Warning($"recipe {__instance.FollowingRecipe.UniqueID}");
+
+                if (__result == null && !CraftGoal__ReversePatch.HasUsedIngredientsWaitingForProduct(__instance, character) && __instance.FollowingRecipe.RequiredPropToWorkOn() != 0)
+                {
+                    GameTerrain terrain = GameTerrain.Instance;
+                    CraftingProp craftingProp = terrain.GetFixedObjectOnTile(__instance.DestTile.x, __instance.DestTile.y) as CraftingProp;
+                    if (craftingProp != null && (craftingProp.CraftingFinished || craftingProp.CraftingRecipe == null)) {
+                        if (craftingProp.Inventory.GetTotalNutrition() > 0f) {
+                            // Main.mod.Logger.Warning("target prop has stuck food");
+                            if (__instance.DesiredAmount == int.MaxValue && character.GetRole() == Role.Cook) {
+                                // Main.mod.Logger.Warning("is cook");
+                                if (CraftGoal__ReversePatch.GetRetrieveFinishedItemGoal(__instance, character, craftingProp, out Goal goal2)) {
+                                    // result_goal_sb.Clear();
+                                    // goal2.BuildDebugString(result_goal_sb);
+                                    // Main.mod.Logger.Warning($"found a new goal: {result_goal_sb.ToString()}");
+                                    return goal2;
+                                } else {
+                                    // Main.mod.Logger.Warning("couldn't find a new goal");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return __result;
+        }
+    }
+
+    [HarmonyPatch(typeof(CraftAnim), "Update")]
+    static class CraftAnim__Update__Patch {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+            var codes = new List<CodeInstruction>(instructions);
+            
+            var StopRecipe_MethodInfo = typeof(CraftGoal).GetMethod("StopRecipe");
+
+            for (int i = codes.Count - 1; i >= 2; i--) {
+                CodeInstruction ci = codes[i];
+                if (ci.Calls(StopRecipe_MethodInfo)) {
+                    codes.RemoveRange(i - 2, 3);
+                    break;
+                }
+            }
+            
+            return codes;
+        }
+    }
+
+    [HarmonyPatch(typeof(CraftGoal), "OnCraftingFinished")]
+    static class CraftGoal__OnCraftingFinished__Patch {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+            var codes = new List<CodeInstruction>(instructions);
+            
+            var StopRecipe_MethodInfo = typeof(CraftGoal).GetMethod("StopRecipe");
+
+            for (int i = 0; i < codes.Count; i++) {
+                CodeInstruction ci = codes[i];
+                if (ci.Calls(StopRecipe_MethodInfo)) {
+                    codes.RemoveRange(i - 2, 3);
+                    break;
+                }
+            }
+            
+            return codes;
+        }
+    }
+}
